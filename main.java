7b926e04bdf3d627d03c0c620a51d6404e56e134
Avilash <96437153+jhaavilash@users.noreply.github.com>
2025-01-02 22:56:04 +0530

    Created main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class SystemInfo {
+    public static void main(String[] args) {
+        // Get current date and time
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = now.format(formatter);
+
+        // Get system information
+        String osName = System.getProperty("os.name");
+        String osVersion = System.getProperty("os.version");
+        String osArch = System.getProperty("os.arch");
+        String javaVersion = System.getProperty("java.version");
+        String userName = System.getProperty("user.name");
+
+        // Print the information
+        System.out.println("Current Date and Time: " + formattedDate);
+        System.out.println("Operating System: " + osName);
+        System.out.println("OS Version: " + osVersion);
+        System.out.println("OS Architecture: " + osArch);
+        System.out.println("Java Version: " + javaVersion);
+        System.out.println("User Name: " + userName);
+    }
+}
